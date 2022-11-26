6a6891ee54e236a1d3313216357ceacf7bd43092
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-26 13:17:25 -0300

    Update 1.1_Linear_Search.rb

--- 1.1_Linear_Search.rb
+++ 1.1_Linear_Search.rb
@@ -1,1 +1,14 @@
-puts "1,2,3 testando..."
+list_number = [10, 20, 80, 30, 60, 50,110, 100, 130, 170]
+
+puts "Type a number to search for"
+n = gets.chomp.to_i
+
+list_number.each do |number|
+  if list_number.include?(n)
+    puts "Element #{n} is present at index #{list_number.index(n)}"
+    break
+  else
+    puts "Element is not in the array"
+    break
+  end
+end
