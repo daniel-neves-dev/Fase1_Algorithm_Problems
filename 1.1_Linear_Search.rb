017218c3ffcd1d99b221ce029ab1c747d7eb98b7
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-26 13:25:32 -0300

    Update 1.1_Linear_Search.rb

--- 1.1_Linear_Search.rb
+++ 1.1_Linear_Search.rb
@@ -1,5 +1,10 @@
 list_number = [10, 20, 80, 30, 60, 50,110, 100, 130, 170]
 
+6.times{print "*"}
+puts "\nLINEAR SEARCH"
+6.times{print "*"}
+puts
+
 puts "Type a number to search for"
 n = gets.chomp.to_i
 
