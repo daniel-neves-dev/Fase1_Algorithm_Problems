bed9927c7cb074898dc588fdf0a67b38a9c07bdd
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-26 17:07:34 -0300

    Update 2.1_Bubble Sort.rb

--- 2.1_Bubble Sort.rb
+++ 2.1_Bubble Sort.rb
@@ -1,11 +1,6 @@
 numbers = [5, 1, 4, 2, 8]
-
-numbers.each do |i|
-  i.each do |j|
-    if numbers[j] > numbers[j+1]
-      numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-      p numbers
-    end
-  end
+code = numbers.length -1
+loop do 
+  print numbers
+  code -1
 end
-  
