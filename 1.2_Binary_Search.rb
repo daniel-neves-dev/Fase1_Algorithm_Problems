bd9b93b0fae97ac9fcc461588c5e01f1f7197da7
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-26 14:01:14 -0300

    Function
    add function (def)

--- 1.2_Binary_Search.rb
+++ 1.2_Binary_Search.rb
@@ -1,27 +1,25 @@
 list_number = [10, 20, 80, 30, 60, 50,110, 100, 130, 170]
+middle = list_number[list_number.length / 2]
+
+def search_number(n, list_number, middle)
+  list_number.each do |number|
+    if n == middle
+      return puts "Element #{n} is present at index #{list_number.index(n)}"
+    elsif n > middle
+      new_middel = list_number[list_number.length+1 / 2]
+      return puts "Element #{n} is present at index #{list_number.index(n)}"
+    elsif n < middle
+      new_middel = list_number[list_number.length-1 / 2]
+      return puts "#{n} index #{list_number.index(n)}"
+    end
+  end
+end
 
 puts "Type a number to serach for"
 n = gets.chomp.to_i 
 
-middle = list_number[list_number.length / 2]
-
-list_number.each do |number|
-  if list_number.include?(n)
-    if n == middle
-      puts "#{n} index #{list_number.index(n)}"
-      break
-
-    elsif n > middle
-      new_middel = list_number[list_number.length+1 / 2]
-      puts "#{n} index #{list_number.index(n)}"
-      break
-
-    elsif n < middle
-      new_middel = list_number[list_number.length-1 / 2]
-      puts "#{n} index #{list_number.index(n)}"
-      break
-    end
-  end
+if list_number.include?(n)
+  search_number(n, list_number, middle)
+else   
   puts "Element #{n} is not present"
-  break
 end
