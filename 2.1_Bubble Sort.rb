4c6d9bebda1e9f6df94088d2e19b8c440d51ff90
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-26 18:18:07 -0300

    Update 2.1_Bubble Sort.rb

--- 2.1_Bubble Sort.rb
+++ 2.1_Bubble Sort.rb
@@ -1,7 +1,7 @@
 numbers = [5, 1, 4, 2, 8]
 
 numbers.each do |i|
-  i.size.times do |j|
+  i.times do |j|
     j = numbers.size-i
     if numbers[j] > numbers[j+1]
       numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
